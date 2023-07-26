13f7cf3582bc75d0f6ccfc7e9856cd4b23320e44
Yann Kamche <yannkamche@gmail.com>
2023-07-26 16:28:01 +0100

    Learnt how to add custom fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+            100: '#E2E2D5',
+            200: '#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Poppins']
+      }
+    },
   },
   plugins: [],
 }
